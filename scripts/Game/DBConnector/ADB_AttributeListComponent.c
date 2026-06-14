e71d6bf9422693f49c014e04d225a23247b18ce5
THEGUY-SR <gavinwine10@gmail.com>
2026-06-14 05:21:53 -0400

    as many fixes as i could

--- scripts/Game/DBConnector/ADB_AttributeListComponent.c
+++
@@ -1,8 +0,0 @@
-[BaseContainerProps()]
-class ADB_AttributeListComponentClass : ScriptComponentClass {}
-
-class ADB_AttributeListComponent : ScriptComponent
-{
-	[Attribute()]
-	ref array<ref ADB_AttributeString> stringAttributes;
-}
--- scripts/Game/UserSetFaction.c
+++ scripts/Game/UserSetFaction.c
@@ -1,44 +1,44 @@
 class SRZ_USER_SET_FACTION : ScriptedUserAction
 {
-// Dropdown in the action’s properties
-[Attribute("1", UIWidgets.ComboBox, "Faction to set", "", ParamEnumArray.FromEnum(ARMST_FACTION_LABEL), category: "Faction")]
-protected ARMST_FACTION_LABEL m_FactionKey;
+	[Attribute("1", UIWidgets.ComboBox, "Faction to set", "", ParamEnumArray.FromEnum(ARMST_FACTION_LABEL), category: "Faction")]
+	protected ARMST_FACTION_LABEL m_FactionKey;
 
-// Optional: delete the object after someone uses it (like your example)
-[Attribute("0", UIWidgets.CheckBox, "Delete entity after use", "If enabled, the entity this action is on will be deleted after use.", category: "Behavior")]
-protected bool m_DeleteOwnerAfterUse = false;
+	[Attribute("0", UIWidgets.CheckBox, "Delete entity after use", "", category: "Behavior")]
+	protected bool m_DeleteOwnerAfterUse;
 
-//------------------------------------------------------------------------------------------------
-override void PerformAction(IEntity pOwnerEntity, IEntity pUserEntity)
-{
-if (!pUserEntity)
-return;
+	//------------------------------------------------------------------------------------------------
+	override void PerformAction(IEntity pOwnerEntity, IEntity pUserEntity)
+	{
+		if (!Replication.IsServer())
+			return;
 
-ARMST_PLAYER_STATS_COMPONENT playerStats =
-ARMST_PLAYER_STATS_COMPONENT.Cast(pUserEntity.FindComponent(ARMST_PLAYER_STATS_COMPONENT));
+		if (!pUserEntity)
+			return;
 
-if (!playerStats)
-return;
+		ARMST_PLAYER_STATS_COMPONENT playerStats =
+			ARMST_PLAYER_STATS_COMPONENT.Cast(pUserEntity.FindComponent(ARMST_PLAYER_STATS_COMPONENT));
 
-playerStats.SetFactionKey(m_FactionKey);
+		if (!playerStats)
+			return;
 
-if (m_DeleteOwnerAfterUse && pOwnerEntity)
-SCR_EntityHelper.DeleteEntityAndChildren(pOwnerEntity);
+		playerStats.SetFactionKey(m_FactionKey);
+
+		Print(string.Format("[SRZ_FACTION] Set faction to %1 for user %2", m_FactionKey, pUserEntity), LogLevel.NORMAL);
+
+		if (m_DeleteOwnerAfterUse && pOwnerEntity)
+			SCR_EntityHelper.DeleteEntityAndChildren(pOwnerEntity);
+	}
+
+	//------------------------------------------------------------------------------------------------
+	override bool GetActionNameScript(out string outName)
+	{
+		outName = "Set Faction";
+		return true;
+	}
+
+	//------------------------------------------------------------------------------------------------
+	override bool CanBePerformedScript(IEntity user)
+	{
+		return true;
+	}
 }
-
-//------------------------------------------------------------------------------------------------
-override bool GetActionNameScript(out string outName)
-{
-outName = "Set Faction";
-return true;
-}
-
-//------------------------------------------------------------------------------------------------
-override bool CanBePerformedScript(IEntity user)
-{
-return true;
-}
-
-//------------------------------------------------------------------------------------------------
-void ~SRZ_USER_SET_FACTION() {}
-};
